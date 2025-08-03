1b08914698ceb0550532b5d6369d3ae15a537b6e
Genaro Toledo <Genaro.Toledo@alu.frt.utn.edu.ar>
2025-08-03 18:56:59 -0300

    - Configuración para la validación del Token en el program.cs - Creación del servicio "JwtTokenService" para la generación del token.

---
+++ Dsw2025Tpi.Application/Services/JwtTokenService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Services;
+
+public class JwtTokenService
+{
+    private readonly IConfiguration _config;
+
+    public JwtTokenService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public string GenerateToken(string username)
+    {
+        var jwtConfig = _config.GetSection("Jwt");
+        var keyText = jwtConfig["Key"] ?? throw new ArgumentNullException("Jwt Key");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(keyText));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            //new Claim("role", role)
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: jwtConfig["Issuer"],
+            audience: jwtConfig["Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(double.Parse(jwtConfig["ExpireInMinutes"] ?? "60")),
+            signingCredentials: creds
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
