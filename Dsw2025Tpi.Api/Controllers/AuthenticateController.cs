9431f2188030e196392e071d7062b8802d14d546
Genaro Toledo <Genaro.Toledo@alu.frt.utn.edu.ar>
2025-08-03 19:14:32 -0300

    - Adición del controlador de autenticación.

---
+++ Dsw2025Tpi.Api/Controllers/AuthenticateController.cs
@@ -0,0 +1,30 @@
+﻿using Dsw2025Tpi.Application.Dtos;
+using Dsw2025Tpi.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2025Tpi.Api.Controllers;
+
+[ApiController]
+[Route("api/authenticate")]
+public class AuthenticateController : ControllerBase
+{
+    private readonly JwtTokenService _jwtTokenService;
+
+    public AuthenticateController(JwtTokenService jwtTokenService)
+    {
+        _jwtTokenService = jwtTokenService;
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login([FromBody] LoginModel request)
+    {
+        if(request.Username == "admin" && request.Password == "1234")
+        {
+            var token = _jwtTokenService.GenerateToken(request.Username);
+            return Ok(new { token });
+        }
+
+        return Unauthorized();
+    }   
+
+}
---
+++ Dsw2025Tpi.Application/Dtos/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Dtos;
+
+public record LoginModel(string Username, string Password);
