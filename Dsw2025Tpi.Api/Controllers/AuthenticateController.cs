12227e83da4cb895d9268ffe42f47bac06a1621f
Genaro Toledo <Genaro.Toledo@alu.frt.utn.edu.ar>
2025-08-03 20:21:50 -0300

    - Modificación del endpoint del login para el controlador de autenticación. - Implementación del endpoint de registro de usuarios; por consecuencia, también surgió la creación de un "RegisterModel".

--- Dsw2025Tpi.Api/Controllers/AuthenticateController.cs
+++ Dsw2025Tpi.Api/Controllers/AuthenticateController.cs
@@ -1,5 +1,6 @@
 ﻿using Dsw2025Tpi.Application.Dtos;
 using Dsw2025Tpi.Application.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dsw2025Tpi.Api.Controllers;
@@ -8,23 +9,51 @@ namespace Dsw2025Tpi.Api.Controllers;
 [Route("api/authenticate")]
 public class AuthenticateController : ControllerBase
 {
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
     private readonly JwtTokenService _jwtTokenService;
 
-    public AuthenticateController(JwtTokenService jwtTokenService)
+    public AuthenticateController(UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager,
+        JwtTokenService jwtTokenService)
     {
+        _userManager = userManager;
+        _signInManager = signInManager;
         _jwtTokenService = jwtTokenService;
     }
 
     [HttpPost("login")]
-    public IActionResult Login([FromBody] LoginModel request)
+    public async Task<IActionResult> Login([FromBody] LoginModel request)
     {
-        if(request.Username == "admin" && request.Password == "1234")
+        var user = await _userManager.FindByNameAsync(request.Username);
+        if (user == null)
         {
-            var token = _jwtTokenService.GenerateToken(request.Username);
-            return Ok(new { token });
+            return Unauthorized("Usuario o contraseña incorrectos");
         }
 
-        return Unauthorized();
-    }   
+        var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+        if (!result.Succeeded)
+        {
+            return Unauthorized("Usuario o contraseña incorrectos");
+        }
+
+        var token = _jwtTokenService.GenerateToken(request.Username);
+        return Ok(new { token });
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterModel model)
+    {
+
+
+        var user = new IdentityUser { UserName = model.Username, Email = model.Email };
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (!result.Succeeded)
+            return BadRequest(result.Errors);
+
+        // Opcional: enviar email de confirmación, etc.
+        return Ok("Usuario registrado correctamente.");
+    }
 
 }
